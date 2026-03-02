a2e2abc0464a6902bb0d3df41dc246e5b6ae656e
Rana-ElFetyany <ranafet777@gmail.com>
2026-03-02 11:18:43 +0200

    Create HelloITI class file

--- obj/Debug/net9.0/Lap1.AssemblyInfo.cs
+++ obj/Debug/net9.0/Lap1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lap1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3a9884c3f041da9d13ab0bd3489bd02a73419cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lap1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lap1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
