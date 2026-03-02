952e142cdccb75149dff08c60bff35bffb2cbf95
Rana-ElFetyany <ranafet777@gmail.com>
2026-03-02 11:26:48 +0200

    Add Test.txt file to the project

--- obj/Debug/net9.0/Lap1.AssemblyInfo.cs
+++ obj/Debug/net9.0/Lap1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lap1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3a9884c3f041da9d13ab0bd3489bd02a73419cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dba5d703a17fb49fca291e291c11b95467ae8bbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lap1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lap1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
